49907908c30c97bd21b07fd02c1f9a23c3fd9dae
yoganugraha24 <yogajebag24@gmail.com>
2024-08-16 00:57:45 +0700

    website hotels

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './css/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
